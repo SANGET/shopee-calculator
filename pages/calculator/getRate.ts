6cb0f422fc69fdee220343622dc0a6812c5bf66b
sanget <zh.sanget@gmail.com>
2020-12-03 22:04:36 +0800

    Update proj

---
+++ pages/calculator/getRate.ts
@@ -0,0 +1,47 @@
+import { RequestClass } from '@mini-code/request'
+
+interface ResStruct {
+  success: string
+  result: {
+    status: string
+    scur: string
+    tcur: string
+    ratenm: string
+    rate: string
+    update: string
+  }
+}
+
+const $R = new RequestClass<ResStruct>();
+
+export interface GetRateApiOptions {
+  scur: string
+  tcur: string
+}
+
+const rateCache: Record<string, string> = {
+
+}
+
+const res = {
+  "success":"1",
+  "result":{
+    "status":"ALREADY",
+    "scur":"USD",
+    "tcur":"CNY",
+    "ratenm":"美元/人民币",
+    "rate":"6.551",
+    "update":"2020-12-03 20:36:24"
+  }
+}
+
+export const getRate = async ({
+  scur, tcur
+}: GetRateApiOptions) => {
+  if(rateCache[scur]) return rateCache[scur];
+  const apiUrl = `http://api.k780.com/?app=finance.rate&scur=${scur}&tcur=${tcur}&appkey=55916&sign=4d064cee9f44223231209ba9ad22e550`
+  const res = await $R.get(apiUrl)
+  if(res.success === '1') {
+    return res.result.rate
+  }
+}
--- pages/calculator/panel.tsx
+++ pages/calculator/panel.tsx
@@ -9,70 +9,97 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 import clsx from 'clsx';
 
-export default class Calculator extends React.Component {
+import { getRate } from './getRate'
+
+const currencyInfo = [
+  {
+    alias: '台湾',
+    scur: 'TWD'
+  },
+  {
+    alias: '台湾',
+    scur: 'IDR'
+  },
+]
+
+const targetCurrencyInfo = [
+  {
+    alias: '人民币',
+    scur: 'CNY'
+  },
+]
+
+interface CalculatorState {
+  buyingPrice?: number
+}
+export default class Calculator extends React.Component<{}, CalculatorState> {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  calc = (e) => {
+    console.log(e);
+  }
+
+  getExchangeRate = () => {
+
+  }
+  
   render() {
+    const { buyingPrice } = this.state
     return (
       <Container maxWidth="sm">
         <Paper className="calculator-container" style={{padding: 20}}>
           <h3>参数输入</h3>
-          <Grid container spacing={3}>
-            <Grid item>
-              <FormControl className={clsx()}>
-                <Input
-                  id="standard-adornment-weight"
-                  value={'values.weight'}
-                  // onChange={handleChange('weight')}
-                  endAdornment={<InputAdornment position="end">Kg</InputAdornment>}
-                  aria-describedby="standard-weight-helper-text"
-                  inputProps={{
-                    'aria-label': 'weight',
-                  }}
-                />
-                <FormHelperText id="standard-weight-helper-text">Weight</FormHelperText>
-              </FormControl>
+          <form action={this.calc}>
+            <Grid container spacing={3}>
+              <Grid item>
+                <TextField label="采购价格" id="buyingPrice" />
+              </Grid>
+              <Grid item>
+                <TextField label="商品重量（克）" id="commodityWeight" />
+              </Grid>
+              <Grid item>
+                <TextField label="手续费%" />
+              </Grid>
+              <Grid item>
+                <TextField label="附加费" />
+              </Grid>
+              <Grid item>
+                <TextField label="利润%" />
+              </Grid>
+              <Grid item>
+                <TextField label="包装重量（克）" />
+              </Grid>
+              <Grid item>
+                <TextField label="折扣%" />
+              </Grid>
+              <Grid item>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={10}
+                  // onChange={handleChange}
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </Grid>
+              <Grid item>
+                <TextField label="汇率" />
+              </Grid>
+              <Grid item>
+                <TextField label="物流" />
+              </Grid>
             </Grid>
-            <Grid item>
-              <TextField label="采购价格" />
-            </Grid>
-            <Grid item>
-              <TextField label="商品重量（克）" />
-            </Grid>
-            <Grid item>
-              <TextField label="手续费%" />
-            </Grid>
-            <Grid item>
-              <TextField label="附加费" />
-            </Grid>
-            <Grid item>
-              <TextField label="利润%" />
-            </Grid>
-            <Grid item>
-              <TextField label="包装重量（克）" />
-            </Grid>
-            <Grid item>
-              <TextField label="折扣%" />
-            </Grid>
-            <Grid item>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={10}
-                // onChange={handleChange}
-              >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </Grid>
-            <Grid item>
-              <TextField label="汇率" />
-            </Grid>
-            <Grid item>
-              <TextField label="物流" />
-            </Grid>
-          </Grid>
+            <Button
+              variant="contained" color="primary" disableElevation
+            >提交</Button>
+          </form>
         </Paper>
         <Paper className="result-area" style={{padding: 20}}>
           <h3>计算结果</h3>
